4947645410bb740201c728fba64bcfbd833a8c98
Mach Hao Tuan <117436192+Huchuynh@users.noreply.github.com>
2025-04-12 19:24:01 +0700

    Update main.py at CHUONG6 TH

--- chuong6/TH/main.py
+++ chuong6/TH/main.py
@@ -17,7 +17,7 @@ app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["127.0.0.0:5500"],  # Cho phép frontend truy cập
-    llow_credentials=True,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
