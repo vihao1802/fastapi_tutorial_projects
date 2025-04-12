f5e117d98c621c48988982b25b065456367d9935
Mach Hao Tuan <117436192+Huchuynh@users.noreply.github.com>
2025-04-12 19:06:04 +0700

    Update main.py

--- chuong6/TH/main.py
+++ chuong6/TH/main.py
@@ -24,3 +24,9 @@ app.add_middleware(
 
 app.include_router(posts.router)
 app.include_router(users.router)
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
